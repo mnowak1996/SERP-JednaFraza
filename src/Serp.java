4a29bc786b5bb234d6ea1b167992d5ff6bfc652d
mnowak1996 <nowakmichal9696@gmail.com>
2018-03-06 22:11:19 +0100

    wersja z ostateczna z komenatrzami

--- src/Serp.java
+++ src/Serp.java
@@ -30,42 +30,42 @@ public class Serp {
     }
 
     public void SprawdzPozycje(String fraza, String domena) {
-        Runnable task = new Runnable() {
+        Runnable task = new Runnable() { // tworzenie watku
             @Override
             public void run() {
                 try {
-                    // buttonSprawdz.setDisable(true);
-                    System.setProperty("webdriver.chrome.driver", "C:\\Users\\nowak\\Selenium\\chromedriver.exe");
-                    WebDriver driver = new ChromeDriver();
-                    driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-                    //driver.manage().window().setPosition(new Point(-2000, 0));
-                    driver.get("https://google.pl/");
-                    Thread.sleep(2000);
-                    String source = driver.getPageSource();
+
+                    System.setProperty("webdriver.chrome.driver", "C:\\Users\\nowak\\Selenium\\chromedriver.exe"); // ustawienie miejsca dodaku do przegladarki chrome
+                    WebDriver driver = new ChromeDriver(); // tworzenie obiektu przegldarki
+                    driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);// czeka 15 sekund, jesli storna sie nie zaladuje to odswieza
+                    driver.manage().window().setPosition(new Point(-2000, 0)); // przeglądarka bdzie schowana
+                    driver.get("https://google.pl/");  // wejscie na strone google.pl
+                    Thread.sleep(4000); // 4 sekundy oczekiwania
+                    String source = driver.getPageSource(); // pobranie kodu zrodlowego strony
                     String pozycja = "Brak poączenia z internetem";
-                    if (source.contains("ERR_INTERNET_DISCONNECTED")) {
+                    if (source.contains("ERR_INTERNET_DISCONNECTED")) { // sprawdzenie czy kod zrodlowy strony zawiera informacje o braku polaczneia z internetem
                         labelPozycja.setText("Brak polaczenia z internetem");
-                        driver.close();
-                        driver.quit();
+                        driver.close(); // zakmniecie przeglądarki
+                        driver.quit();  // wylaczenie przegladarki
 
                     } else {
-                        driver.get("https://cmonitor.pl/sprawdz-pozycje");
+                        driver.get("https://cmonitor.pl/sprawdz-pozycje");// wejscie na strone cmonitor.pl
                         //driver.navigate().refresh();
-                        driver.findElement(By.xpath("//*[@id='phrases']")).sendKeys(textFraza.getText());
-                        driver.findElement(By.xpath("//*[@id='domain']")).sendKeys(textDomena.getText());
-                        driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/form/div[2]/a")).click();
-                        Thread.sleep(6000);
-                        pozycja = driver.findElement(By.xpath("//*[@id='result_0']")).getText();
-                        driver.close();
-                        driver.quit();
+                        driver.findElement(By.xpath("//*[@id='phrases']")).sendKeys(textFraza.getText()); // wpisanie frazy
+                        driver.findElement(By.xpath("//*[@id='domain']")).sendKeys(textDomena.getText()); // wpisanie nazwy domeny
+                        driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/form/div[2]/a")).click(); // klikniecie przyciksku sprawdz pozycje
+                        Thread.sleep(6000); // 6 sekund oczekiwania
+                        pozycja = driver.findElement(By.xpath("//*[@id='result_0']")).getText(); // pobranie pozycji
+                        driver.close(); // zakmniecie przeglądarki
+                        driver.quit();   // wylaczenie przegladarki
                     }
 
                     String finalPozycja = pozycja;
                     Platform.runLater(new Runnable() {
                         @Override
-                        public void run() {
-                            labelPozycja.setText("Pozycja: " + finalPozycja);
-                            System.out.print(finalPozycja);
+                        public void run() {  // watek odpowiedzialny za komunikacje z GUI
+                            labelPozycja.setText("Pozycja: " + finalPozycja);  // wypisanie pozycji
+                           // System.out.print(finalPozycja);
 
                         }
                     });
@@ -75,7 +75,7 @@ public class Serp {
             }
         };
         try {
-            new Thread(task).start();
+            new Thread(task).start(); // rozpoczecie dzialania watku
         } catch (Exception ex) {
             System.out.print(ex.getMessage());
         }
